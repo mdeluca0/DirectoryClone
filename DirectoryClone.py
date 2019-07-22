7f37ffdab9dfa269e81f8b71c6f4ac638b85bce9
Mike DeLuca <32890117+mdeluca0@users.noreply.github.com>
2019-07-22 10:43:24 -0600

    Update DirectoryClone.py

--- DirectoryClone.py
+++ DirectoryClone.py
@@ -1,9 +1,9 @@
 import os
 
-#Path to directory to be cloned
+# Path to directory to be cloned
 target = ""
 
-#Path to directory to be created
+# Path to directory to be created
 clone = ""
 
 print("Cloning \"" + target + "\" to \"" + clone + "\"")
@@ -14,7 +14,7 @@ for (root, dirs, files) in os.walk(target):
 	for name in files:
 		path = os.path.join(root, name);
 		path = path.replace(target, clone, 1)
-		f=open(path, "w+")
+		f = open(path, "w+")
 		f.close()
 		print("Created file: " + path)
 	for name in dirs:
